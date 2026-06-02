d04579d5d32f4cbfabb4c1f5fe4ec35a8a0f57af
KBinduVarshitha <binduvarshitha25@gmail.com>
2026-06-02 12:21:30 +0530

    Added route optimization endpoint and service

--- src/main/java/fleetmanagement/controller/RouteController.java
+++ src/main/java/fleetmanagement/controller/RouteController.java
@@ -1,6 +1,7 @@
 package fleetmanagement.controller;
 
 import fleetmanagement.entity.Route;
+import fleetmanagement.service.RouteOptimizationService;
 import fleetmanagement.service.RouteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,9 @@ public class RouteController {
     @Autowired
     private RouteService routeService;
 
+    @Autowired
+    private RouteOptimizationService routeOptimizationService;
+
     @GetMapping
     public List<Route> getAllRoutes() {
         return routeService.getAllRoutes();
@@ -22,5 +26,10 @@ public class RouteController {
     @PostMapping
     public Route addRoute(@RequestBody Route route) {
         return routeService.saveRoute(route);
+    }
+
+    @PostMapping("/optimize")
+    public List<String> optimizeRoute(@RequestBody List<String> stops) {
+        return routeOptimizationService.optimizeRoute(stops);
     }
 }
